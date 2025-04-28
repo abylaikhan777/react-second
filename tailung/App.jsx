de1bd0e6f41dcadd17a523004992a05792260bc4
tyrimbet abylaikhan <abeke.618@gmail.com>
2025-04-28 18:53:25 +0500

    done

---
+++ tailung/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+export default function App() {
+    return(
+        <div className="ml-30.5 " >
+            <header>
+                <h1 className="text-amber-600  ">dasdf</h1>
+                <pd>lsAD;FLAS;DLFK</pd>
+            </header>
+            <main className="grid grid-cols-3 ">
+                <div className="w-90 h-90">
+                    <h1>fly.ru</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, molestiae? Laboriosam ab sequi inventore doloribus, sint veniam. Soluta quaerat sapiente voluptate expedita corporis perspiciatis consequuntur commodi itaque cum voluptates? Esse.</p>
+                    <button>limited</button>
+                </div>
+                <div className="w-90 h-90">
+                    <h1>maximum</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consectetur, nulla praesentium illum quisquam culpa labore ipsam vitae, assumenda voluptas doloremque fugit aspernatur tempora molestiae, corporis illo ex magni ratione.</p>
+                    <button>limited</button>
+                </div>
+                <div className="w-90 h-90">
+                    <h1>contur</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, magnam nemo. Voluptatibus vero iure porro voluptatum dolorem, doloribus facilis doloremque explicabo numquam possimus dolor iusto architecto placeat cumque facere molestiae.</p>
+                    <button>limited</button>
+                </div>
+                <div className="w-90 h-90">
+                    <h1>nauka</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia sit cupiditate, natus saepe quia cumque atque delectus explicabo qui dicta quasi reiciendis iste dignissimos, unde sint. Magni reprehenderit eaque ex?</p>
+                    <button>limited</button>
+                </div>
+                <div className="w-90 h-90">
+                    <h1>somthing</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique earum dolor nobis ea culpa ipsam quos illum, voluptatibus beatae possimus excepturi dignissimos ex. Explicabo, velit ex hic fugiat debitis iure!</p>
+                    <button>limited</button>
+                </div>
+                <div className="bg-black ">
+                    <h1 className="text-blue-50  w-2xs">and also 20 more copany</h1>    
+                </div>
+                
+                
+            </main>
+        </div>
+    )
+}
